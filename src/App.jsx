82d45788f930d6b625351626f5ebde7f4151d149
harzeezco <wakilabdulazeez@gmail.com>
2023-10-22 20:18:43 +0100

    Rounding up on the config

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-import { useState } from "react";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div></div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <div className="card flex justify-center">
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
